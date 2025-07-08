767374e54c9f2c559712e9ff98d68b9588e462cf
sw-laptop <drone1575@gmail.com>
2025-07-08 16:45:18 +0900

    Add ProgramModule Class -> Manage external program

---
+++ SimulatorRecorder/Modules/ProgramModule.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace SimulatorRecorder.Modules
+{
+    static class ProgramModule
+    {
+        static private Process? process;
+
+        static public void Launch(string[] args)
+        {
+            process = new Process();
+            process.StartInfo.FileName = MyConstant.unityDirectoryPath;
+            process.StartInfo.Arguments = string.Join(" ", args);
+            process.Start();
+        }
+
+        static public void Close()
+        {
+            if (process != null && !process.HasExited)
+            {
+                process.CloseMainWindow();
+                if (!process.WaitForExit(3000))
+                {
+                    process.Kill();
+                    process.WaitForExit();
+                }
+            }
+        }
+    }
+}
