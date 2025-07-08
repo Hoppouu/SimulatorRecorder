fd5f1ec4541b835e32b4d6953c89154db63a30e1
sw-laptop <drone1575@gmail.com>
2025-07-08 16:26:25 +0900

    Fix port number and conver to static function

--- SimulatorRecorder/Modules/HotKeyModule.cs
+++ SimulatorRecorder/Modules/HotKeyModule.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 using System.Text;
 
-public class HotKeyModule
+public static class HotKeyModule
 {
     [DllImport("user32.dll")]
     public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
@@ -14,19 +14,19 @@ public class HotKeyModule
     private const uint VK_SPACE = 0x20;
     private const uint MOD_NONE = 0x0000;
 
-    public event Action OnHotKeyPressed = null!;
+    public static event Action OnHotKeyPressed = null!;
 
-    public void RegisterHotKey(IntPtr hWnd)
+    public static void RegisterHotKey(IntPtr hWnd)
     {
         RegisterHotKey(hWnd, HOTKEY_ID, MOD_NONE, VK_SPACE);
     }
 
-    public void UnregisterHotKey(IntPtr hWnd)
+    public static void UnregisterHotKey(IntPtr hWnd)
     {
         UnregisterHotKey(hWnd, HOTKEY_ID);
     }
 
-    public void ProcessHotKeyMessage(ref Message m)
+    public static void ProcessHotKeyMessage(ref Message m)
     {
         const int WM_HOTKEY = 0x0312;
         if (m.Msg == WM_HOTKEY && (int)m.WParam == HOTKEY_ID)
@@ -35,10 +35,10 @@ public class HotKeyModule
         }
     }
 
-    public void SendHotKeySignal()
+    public static void SendHotKeySignal()
     {
         using var udpClient = new UdpClient();
         byte[] data = Encoding.UTF8.GetBytes("SPACE_PRESSED");
-        udpClient.Send(data, data.Length, "127.0.0.1", 12345);
+        udpClient.Send(data, data.Length, "127.0.0.1", 55555);
     }
 }
